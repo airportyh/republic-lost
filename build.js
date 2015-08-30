737314d63d36e32474e98a68b0a25fe4006f1d64
Toby Ho <airportyh@gmail.com>
2015-08-30 08:30:30 -0400

    Better readability for mobile devices.

--- build.js
+++ build.js
@@ -260,6 +260,7 @@ function template(text) {
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="stylesheet.css" rel="stylesheet">
 </head>
 <body>
